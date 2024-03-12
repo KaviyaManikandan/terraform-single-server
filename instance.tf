4d41b059ef6e6a1c62ccb0f9ea6248e024ef3495
KaviyaManikandan <100072155+KaviyaManikandan@users.noreply.github.com>
2024-03-12 14:27:10 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,40 +1,36 @@
-provider  "aws" {
-    region = "us-east-2"
+provider "aws" {
+  region = "us-east-2"
 }
 
 resource "aws_instance" "example" {
-  ami                    = "ami-0fb653ca2d3203ac1"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.sg.id,"sg-0840dbb05a887d769"]
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hii kaviya" > index.html
-              nohup busybox httpd -f -p 80 &
-              EOF
+  ami           = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
 
-  user_data_replace_on_change = true
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Hii kaviya" > index.html
+    nohup busybox httpd -f -p 80 &
+    EOF
 
   tags = {
     Name = "terraform-example"
   }
-
 }
 
 resource "aws_security_group" "sg" {
   name = "terraform-example-instance"
+
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-    egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  
-}
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
