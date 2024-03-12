4716766aaa456bbce05abdb61bf8f85c8ecdd377
KaviyaManikandan <100072155+KaviyaManikandan@users.noreply.github.com>
2024-03-12 20:49:54 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -5,6 +5,12 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  subnet_id     = "subnet-00dfb4d67ac08ffbe"  # Specify your subnet ID here
+
+  tags = {
+    Name = "terraform-example"
+  }
 
   user_data = <<-EOF
     #!/bin/bash
@@ -19,6 +25,7 @@ resource "aws_instance" "example" {
 
 resource "aws_security_group" "sg" {
   name = "terraform-example-instance"
+  vpc_id = "vpc-0a4318010bc59a847"  # Specify your VPC ID here
 
   ingress {
     from_port   = 80
